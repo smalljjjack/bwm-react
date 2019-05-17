3b917f4c71de0c8365b6bbbe86df90582b1fc333
Jack <smalljjjack@users.noreply.github.com>
2019-05-17 17:04:17 -0400

    rental list done

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './shared/Header';
 import { RentalCard } from './components/rental/RentalCard';
+import { RentalList } from './components/rental/rental-list';
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
         <div className='container'>
-          <section id='rentalListing'>
-            <h1 className='page-title'>Your Home All Around the World</h1>
-            <div className='row'>
-
-                  <RentalCard />
-                  <RentalCard />
-                  <RentalCard />
-
-            </div>
-          </section>
+          <RentalList />
         </div>
 
       </div>
