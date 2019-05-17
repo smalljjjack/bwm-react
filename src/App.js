ca41225f2a746985f248f7b76898355d629f0636
Jack <smalljjjack@users.noreply.github.com>
2019-05-17 16:45:03 -0400

    rental card done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './shared/Header';
-
+import { RentalCard } from './components/rental/RentalCard';
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <div className='container'>
+          <section id='rentalListing'>
+            <h1 className='page-title'>Your Home All Around the World</h1>
+            <div className='row'>
+
+                  <RentalCard />
+                  <RentalCard />
+                  <RentalCard />
+
+            </div>
+          </section>
+        </div>
+
       </div>
     );
   }
