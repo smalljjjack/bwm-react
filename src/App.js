60b5fdb2df2d46b316b5a3c04746aa5a4bfac8d6
Jack <smalljjjack@users.noreply.github.com>
2019-05-18 22:25:56 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './shared/Header';
 import { RentalCard } from './components/rental/RentalCard';
 import { RentalList } from './components/rental/rental-list';
+import { RentalDetail } from './components/rental/rental-detail';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <div className='container'>
-          <RentalList />
-        </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <div className='container'>
+            <Route exact path='/rentals' component={RentalList} />
+            <Route exact path='/rentals/:id' component={RentalDetail} />
+          </div>
 
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/rental/rental-detail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class RentalDetail extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Im detail. Im detail</h1>
+        <h1>Im detail. Im detail</h1>
+        <h1>Im detail. Im detail</h1>
+        <h1>Im detail. Im detail</h1>
+        <h1>Im detail. Im detail</h1>
+        <h1>Im detail. Im detail</h1>
+      </div>
+    )
+  }
+}
